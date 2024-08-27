d1e38c5c40c558785c2ba70768c18a342effa8f8
Paulina Villarroel <paulina.villarroel@usach.cl>
2024-08-27 19:57:00 -0400

    first commit

---
+++ components/CartWidget/CarWidget.jsx
@@ -0,0 +1,12 @@
+import cart from './assets/shopping_cart.svg'
+
+const CarWidget = () => {
+    return (
+        <div>
+            <img src={cart} alt="cart-widget"/>
+            0
+        </div>
+    )
+}
+
+export default CarWidget;
