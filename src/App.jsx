588ce6a177aea221a4d17bac15c82bb4dfd26272
Paulina Villarroel <paulina.villarroel@usach.cl>
2024-09-18 00:54:46 -0300

    NavegaLasRutas+Villarroel

--- src/App.jsx
+++ src/App.jsx
@@ -43,18 +43,21 @@ import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import NavBar from '../components/Navbar/Navbar.jsx';
 import ItemListContainer from '../components/ItemListContainer/ItemListContainer.jsx';
 import ItemDetailContainer from '../components/ItemDetailContainer/ItemDetailContainer.jsx';
+import { CartProvider } from '../context/CartContext.jsx';
 
 function App(){
     return (
         <div className="App">
           <BrowserRouter>
-            <NavBar />
-            <Routes>
-              <Route path='/' element={<ItemListContainer/>}/>
-              <Route path='/category/:categoryId' element={<ItemListContainer/>}/>
-              <Route path='/item/:itemId' element={<ItemDetailContainer/>}/>
-              <Route path='*' element={<h1>404 NOT FOUND</h1>}/>
-            </Routes>
+            <CartProvider>
+              <NavBar />
+              <Routes>
+                <Route path='/' element={<ItemListContainer/>}/>
+                <Route path='/category/:categoryId' element={<ItemListContainer/>}/>
+                <Route path='/item/:itemId' element={<ItemDetailContainer/>}/>
+                <Route path='*' element={<h1>404 NOT FOUND</h1>}/>
+              </Routes>
+            </CartProvider>
           </BrowserRouter>
         </div>
     );
